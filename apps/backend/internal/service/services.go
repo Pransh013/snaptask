532bf36e84b4f13498c7a0400a14dcc01b1e52be
Pranshu <pranshuverma1601@gmail.com>
2025-08-27 02:00:19 +0530

    add service methods

--- apps/backend/internal/service/services.go
+++ apps/backend/internal/service/services.go
@@ -7,15 +7,21 @@ import (
 )
 
 type Services struct {
-	Auth *AuthService
-	Job  *job.JobService
+	Auth     *AuthService
+	Job      *job.JobService
+	Todo     *TodoService
+	Category *CategoryService
+	Comment  *CommentService
 }
 
 func NewServices(s *server.Server, repos *repository.Repositories) (*Services, error) {
 	authService := NewAuthService(s)
 
 	return &Services{
-		Job:  s.Job,
-		Auth: authService,
+		Job:      s.Job,
+		Auth:     authService,
+		Todo:     NewTodoService(s, repos.Todo, repos.Category),
+		Category: NewCategoryService(s, repos.Category),
+		Comment:  NewCommentService(s, repos.Comment, repos.Todo),
 	}, nil
 }
